5cb7af28cad432f573d95bad88f930fd306b8682
Jose Torreblanca <josewhitetower@gmail.com>
2020-01-26 23:05:46 +0100

    added routes

--- components/Drawer.vue
+++ components/Drawer.vue
@@ -1,19 +1,60 @@
 <template>
-  <div class="bg-gray-900 inset-0 fixed">
+  <div class="bg-gray-900 inset-0 fixed text-white ">
     <div
       class="container flex justify-end px-6 mt-10 md:mt-20 lg:mt-20 mx-auto"
     >
       <font-awesome-icon
         :icon="['fas', 'times']"
         @click="$emit('close')"
-        class="cursor-pointer md:text-xl lg:text-2xl text-white mt-1 mr-1"
+        class="cursor-pointer md:text-xl lg:text-2xl mt-1 mr-1"
       />
     </div>
+    <nav class="Navigation mt-40 flex flex-col items-center justify-center">
+      <ul>
+        <li
+          v-for="route in routes"
+          :key="route.path"
+          class="text-4xl mb-10 text-gray-600 hover:text-white"
+        >
+          <nuxt-link
+            :to="route.path"
+            :exact="route.exact"
+            active-class="text-white"
+            >{{ route.name }}</nuxt-link
+          >
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      routes: [
+        {
+          path: '/',
+          exact: true,
+          name: 'Home'
+        },
+        {
+          path: '/about',
+          name: 'About'
+        }
+      ]
+    }
+  },
+  watch: {
+    '$route.path'() {
+      this.$emit('close')
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+.Navigation {
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,5 @@
+<template> <h1>about</h1> </template>
+<script>
+export default {}
+</script>
+<style></style>
