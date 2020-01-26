ca8798f6499313689dbe1da4e6af1186ba9d6bf2
Jose Torreblanca <josewhitetower@gmail.com>
2020-01-26 21:43:13 +0100

    added transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container px-6 mt-10 md:mt-20 lg:mt-20 mx-auto">
     <Navbar @show="isDrawerVisible = true" />
-    <Drawer v-if="isDrawerVisible" @hide="isDrawerVisible = false" />
+    <transition name="fade">
+      <Drawer v-if="isDrawerVisible" @hide="isDrawerVisible = false" />
+    </transition>
+
     <nuxt />
   </div>
 </template>
@@ -23,4 +26,12 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
