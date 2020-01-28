9cb80e1fb85b3555e8769b49ddd6473e4d2eefbe
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-01-28 17:18:06 +0100

    removed main component

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,30 @@
 <template>
   <div class>
-    <Main />
+    <div class="lg:mt-40 md:mt-32 mt-20">
+      <img
+        src="~assets/imgs/jose.jpeg"
+        alt="Profile picture"
+        class="h-40 w-40 responsive rounded-full mx-auto"
+      />
+      <div
+        class="md:text-center lg:text-center lg:mt-32 md:mt-24 mt-20 text-2xl md:px-20 lg:px-40"
+      >
+        <h1 class="md:text-3xl lg:text-4xl font-sans font-bang text-gray-900">
+          Hi, I'm José Torreblanca &#128075;&#127997;
+        </h1>
+        <p
+          class="text-gray-800 font-sans md:px-6 lg:px-6 mt-4 lg:text-2xl md:text-2xl text-lg"
+        >
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+          eos natus sunt velit atque cumque facere repellendus quo suscipit!
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Main from '../components/Main'
-
-export default {
-  components: {
-    Main
-  }
-}
+export default {}
 </script>
 
 <style>
