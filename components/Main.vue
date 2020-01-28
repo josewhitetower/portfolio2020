38230938ea357b20412b860d02dae2e8d9f2afdd
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-01-28 17:14:16 +0100

    added emoji and fixed font size

--- components/Main.vue
+++ components/Main.vue
@@ -6,12 +6,14 @@
       class="h-40 w-40 responsive rounded-full mx-auto"
     />
     <div
-      class="md:text-center lg:text-center lg:mt-32 md:mt-24 mt-20 lg:text-2xl md:text-2xl text-lg"
+      class="md:text-center lg:text-center lg:mt-32 md:mt-24 mt-20 text-2xl md:px-20 lg:px-40"
     >
       <h1 class="md:text-3xl lg:text-4xl font-sans font-bang text-gray-900">
-        Hi, I'm José Torreblanca
+        Hi, I'm José Torreblanca &#128075;&#127997;
       </h1>
-      <p class="text-gray-800 font-sans md:px-6 lg:px-6 mt-4">
+      <p
+        class="text-gray-800 font-sans md:px-6 lg:px-6 mt-4 lg:text-2xl md:text-2xl text-lg"
+      >
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore eos
         natus sunt velit atque cumque facere repellendus quo suscipit!
       </p>
