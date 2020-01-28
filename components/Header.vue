574fe2ff4f0c4dba4cc8897521227fb812af18e9
Jose Torreblanca <josewhitetower@gmail.com>
2020-01-28 23:51:15 +0100

    added open drawer on keydown

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,13 @@
 <template>
-  <header class="font-sans justify-between flex items-center  text-gray-900">
+  <header class="font-sans justify-between flex items-center text-gray-900">
     <nuxt-link exact to="/">
       <h1 class="md:text-xl lg:text-2xl font-light">José Torreblanca</h1>
     </nuxt-link>
     <font-awesome-icon
       :icon="['fas', 'bars']"
       @click="$emit('open')"
+      @keydown.enter="$emit('open')"
+      tabindex="0"
       class="cursor-pointer md:text-xl lg:text-2xl"
     />
   </header>
