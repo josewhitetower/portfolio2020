2dc648ee191e2b94584c236b5fe44fa5a39362e3
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-01-28 17:53:35 +0100

    changed image size

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <img
       src="~assets/imgs/jose.jpeg"
       alt="Profile picture"
-      class="h-40 w-40 responsive rounded-full mx-auto"
+      class="h-32 md:h-40 lg:h-40 w-32 md:w-40 lg:w-40 responsive rounded-full mx-auto"
     />
     <div
       class="md:text-center lg:text-center lg:mt-32 md:mt-24 mt-20 text-2xl md:px-20 lg:px-40"
@@ -12,7 +12,7 @@
         Hi, I'm José Torreblanca &#128075;&#127997;
       </h1>
       <p
-        class="text-gray-800 font-sans md:px-6 lg:px-6 mt-4 lg:text-2xl md:text-2xl text-lg"
+        class="text-gray-700 font-sans md:px-6 lg:px-6 mt-4 lg:text-2xl md:text-2xl text-lg leading-loose"
       >
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore eos
         natus sunt velit atque cumque facere repellendus quo suscipit!
