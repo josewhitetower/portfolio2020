760ec91b8c7f85364766fcef445f41b45fa711d0
Jose Torreblanca <josewhitetower@gmail.com>
2020-01-29 23:34:24 +0100

    changed fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ export default {
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
-    // 'nuxt-fontawesome'
+    'nuxt-fontawesome',
     // font awesome
     [
       'nuxt-fontawesome',
