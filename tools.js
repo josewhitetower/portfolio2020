4a1425ad6c27b99582efb96090fa5bf4190dae88
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-05 22:55:09 +0100

    added tools

---
+++ tools.js
@@ -0,0 +1,79 @@
+export default {
+  1: [
+    {
+      icon: 'html-5',
+      name: 'HTML5',
+      link: 'https://html.spec.whatwg.org/multipage/'
+    },
+    {
+      icon: 'css-3',
+      name: 'CSS3',
+      link: 'https://www.w3.org/TR/CSS/'
+    },
+    {
+      icon: 'javascript',
+      name: 'JavaScript',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+    }
+  ],
+  2: [
+    {
+      icon: 'vue',
+      name: 'VueJs',
+      link: 'https://vuejs.org/'
+    },
+    {
+      icon: 'react',
+      name: 'ReactJS',
+      link: 'https://reactjs.org/'
+    }
+  ],
+  3: [
+    {
+      icon: 'tailwindcss',
+      name: 'TailwindCSS',
+      link: 'https://tailwindcss.com/'
+    },
+    {
+      icon: 'sass',
+      name: 'Sass',
+      link: 'https://sass-lang.com/'
+    }
+  ],
+  4: [
+    {
+      icon: 'nodejs',
+      name: 'NodeJS',
+      link: 'https://nodejs.org/'
+    },
+    {
+      icon: 'npm',
+      name: 'NPM',
+      link: 'https://www.npmjs.com/'
+    }
+  ],
+  5: [
+    {
+      icon: 'netlify',
+      name: 'Netlify',
+      link: 'https://www.netlify.com/'
+    },
+    {
+      icon: 'travis-ci',
+      name: 'Travis',
+      link: 'https://travis-ci.org/'
+    }
+  ],
+  6: [
+    {
+      icon: 'git',
+      name: 'Git',
+      link: 'https://git-scm.com/'
+    },
+    {
+      icon: 'docker-icon',
+      name: 'Docker',
+      link: 'https://www.docker.com/'
+    }
+  ]
+}
