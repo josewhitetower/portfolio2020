10f3f0787750431c65adc30608e28b4ede7468b7
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-07 15:30:30 +0100

    fixed anchor rel tag

--- experiences.js
+++ experiences.js
@@ -7,7 +7,7 @@ export default {
     },
     duration: '09/2017 - Today',
     tasks:
-      '<p>Help with the conception and development of various publishing news web projects (<a href="https://www.spiegel.de" target="_blank" rel ="noopener noreferrer nofollow">Der Spiegel</a>, <a href="https://www.bento.de" target="_blank" rel ="nofollow">Bento</a>, <a href="https://www.manager-magazin.de/premium/" target="_blank" rel ="nofollow">Manager Magazin Premium</a>) in a dedicated, agile team. Implemented responsive and functional layouts and worked in close cooperation with editors and mobile and backend developers.</p>',
+      '<p>Help with the conception and development of various publishing news web projects (<a href="https://www.spiegel.de" target="_blank" rel="noopener noreferrer nofollow">Der Spiegel</a>, <a href="https://www.bento.de" target="_blank" rel="noopener noreferrer nofollow">Bento</a>, <a href="https://www.manager-magazin.de/premium/" target="_blank" rel="noopener noreferrer nofollow">Manager Magazin Premium</a>) in a dedicated, agile team. Implemented responsive and functional layouts and worked in close cooperation with editors and mobile and backend developers.</p>',
     keywords: ['VueJs', 'TailwindCss', 'ComponentLoaderJs']
   },
   2: {
@@ -18,7 +18,7 @@ export default {
     },
     duration: '11/2016 - 08/2017',
     tasks:
-      '<p>Implemented and helped in the conception of new functionalities for <a href="https://www.dvinci.de/bewerbermanagement-software-dvinci" target="_blank" rel ="noopener noreferrer nofollow">d.vinci</a> and also customer-specific adjustments and extensions for it. It also included analysis and correction of software errors in cooperation with the customer Service using the <a href="https://www.atlassian.com/software/jira" target="_blank" rel ="noopener noreferrer nofollow">JIRA</a> ticket system.</p>',
+      '<p>Implemented and helped in the conception of new functionalities for <a href="https://www.dvinci.de/bewerbermanagement-software-dvinci" target="_blank" rel="noopener noreferrer nofollow">d.vinci</a> and also customer-specific adjustments and extensions for it. It also included analysis and correction of software errors in cooperation with the customer Service using the <a href="https://www.atlassian.com/software/jira" target="_blank" rel="noopener noreferrer nofollow">JIRA</a> ticket system.</p>',
     keywords: ['Perl', 'Boostrap', 'Javascript']
   },
   3: {
@@ -29,7 +29,7 @@ export default {
     },
     duration: '07/2016 - 10/2017',
     tasks:
-      '<p>Working with <a href="https://selenium.dev/" target="_blank" rel ="noopener noreferrer nofollow">Selenium</a> ensured the functionality of the software on all common browsers. Executed versions increases (updates) and applied version control with <a href="https://subversion.apache.org/" target="_blank" rel ="noopener noreferrer nofollow">Subversion</a>.</p>',
+      '<p>Working with <a href="https://selenium.dev/" target="_blank" rel="noopener noreferrer nofollow">Selenium</a> ensured the functionality of the software on all common browsers. Executed versions increases (updates) and applied version control with <a href="https://subversion.apache.org/" target="_blank" rel="noopener noreferrer nofollow">Subversion</a>.</p>',
     keywords: ['Selenium', 'Subversion']
   }
 }
