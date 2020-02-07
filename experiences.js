0a09e9ec45bb51ae5cec13022cb3e14ec8960a1b
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-07 14:22:22 +0100

    added experience data

---
+++ experiences.js
@@ -0,0 +1,32 @@
+export default {
+  1: {
+    role: 'Frontend software developer',
+    company: {
+      name: 'Spiegel TechLab GmbH',
+      link: 'https://www.spiegel-techlab.de'
+    },
+    duration: '09/2017 - Today',
+    tasks:
+      '<p>Helped with the conception and development of various publishing news web projects (<a href="https://www.spiegel.de" target="_blank" rel ="noopener noreferrer nofollow">Der Spiegel</a>, <a href="https://www.bento.de" target="_blank" rel ="nofollow">Bento</a>, <a href="https://www.manager-magazin.de/premium/" target="_blank" rel ="nofollow">Manager Magazin Premium</a>) in a dedicated, agile team. Implemented responsive and functional layouts and worked in close cooperation with editors and mobile and backend developers.</p>'
+  },
+  2: {
+    role: 'Software Developer',
+    company: {
+      name: 'd.vinci HR-Systems GmbH ',
+      link: 'https://www.dvinci.de/'
+    },
+    duration: '11/2016 - 08/2017',
+    tasks:
+      '<p>Implemented and helped in the conception of new functionalities for <a href="https://www.dvinci.de/bewerbermanagement-software-dvinci" target="_blank" rel ="noopener noreferrer nofollow">d.vinci</a> and also customer-specific adjustments and extensions for it. It also included analysis and correction of software errors in cooperation with the customer Service using the <a href="https://www.atlassian.com/software/jira" target="_blank" rel ="noopener noreferrer nofollow">JIRA</a> ticket system.</p>'
+  },
+  3: {
+    role: 'Internship as Software Developer',
+    company: {
+      name: 'd.vinci HR-Systems GmbH ',
+      link: 'https://www.dvinci.de/'
+    },
+    duration: '07/2016 - 10/2017',
+    tasks:
+      '<p>Working with <a href="https://selenium.dev/" target="_blank" rel ="noopener noreferrer nofollow">Selenium</a> ensured the functionality of the software on all common browsers. Executed versions increases (updates) and applied version control with Subversion.</p>'
+  }
+}
--- pages/experience/index.vue
+++ pages/experience/index.vue
@@ -1,7 +1,41 @@
 <template>
-  <h1>experience</h1>
+  <div class="flex">
+    <div>
+      <h1>experience</h1>
+    </div>
+    <ul>
+      <li v-for="(value, key) in experiences" :key="key">
+        <h4>{{ value.role }}</h4>
+        <div>
+          <a
+            :href="value.company.link"
+            :title="value.company.name"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            <span>{{ value.company.name }}</span>
+          </a>
+          |
+          <span>{{ value.duration }}</span>
+        </div>
+        <div v-html="value.tasks" class="tasks"></div>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
-export default {}
+import experiences from '@/experiences'
+export default {
+  head() {
+    return {
+      title: 'Experience'
+    }
+  },
+  data() {
+    return {
+      experiences
+    }
+  }
+}
 </script>
 <style></style>
