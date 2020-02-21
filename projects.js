9ba3f807ab7108b5727fc0735da824dd83d65eed
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-21 22:58:58 +0100

    added project one

---
+++ projects.js
@@ -0,0 +1,11 @@
+export default {
+  1: {
+    title: 'Jetflix',
+    type: 'Full Stack',
+    description:
+      'A web application that consume movies from <a href="https://www.themoviedb.org/about" rel="nofoollow noopener" target="_blank" >The Movie Database (TMDb)</a> through a GraphQL <a href="https://jt-jetflix.appspot.com/graphql" rel="nofoollow noopener" target="_blank" >server</a >. It allows users to search, bookmark and save movies as favorites.',
+    stack: 'Nuxt.js, TailwindCSS, GraphQL',
+    liveDemo: 'https://jt-jetflix.surge.sh',
+    sourceCode: 'https://github.com/josewhitetower/jetflix'
+  }
+}
