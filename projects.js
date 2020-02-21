2699b2d2f739e170cf34655598edef216283d17b
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-21 23:20:32 +0100

    added project 3

--- projects.js
+++ projects.js
@@ -7,5 +7,23 @@ export default {
     stack: 'Nuxt.js, TailwindCSS, GraphQL',
     liveDemo: 'https://jt-jetflix.surge.sh',
     sourceCode: 'https://github.com/josewhitetower/jetflix'
+  },
+  2: {
+    title: 'Auth',
+    type: 'Full Stack',
+    description:
+      'A web portal where users can register, log in and manage their accounts. The backend RESTfull API can be consumed from websites, mobile apps, or any other web service and some integrations test are performed using <a href="https://jestjs.io/" rel="nofoollow noopener" target="_blank" >Jest</a>.',
+    stack: 'React.js, Redux, MaterializeCSS, Express.js, Jest, Docker',
+    liveDemo: '',
+    sourceCode: 'https://github.com/josewhitetower/auth'
+  },
+  3: {
+    title: 'Todo Vue Composition',
+    type: 'Frontend',
+    description:
+      "A Single Page Application to manage and filter tasks using the Vue.js composition API. Items can be dragged and will be storaged using the browser's local Storage. Deployed using <a href='https://www.netlify.com/' rel='nofoollow noopener' target='_blank' >Netlify</a>",
+    stack: 'Vue.js, TailwindCSS, Local Storage',
+    liveDemo: 'https://todo-vue-composition.netlify.com',
+    sourceCode: 'https://github.com/josewhitetower/todo-vue-composition'
   }
 }
