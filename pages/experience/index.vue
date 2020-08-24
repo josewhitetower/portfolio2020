ebf5e9602842089c361dc5b5e8e6d3647e092adf
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-08-24 14:42:18 +0200

    updated cv link

--- pages/experience/index.vue
+++ pages/experience/index.vue
@@ -8,7 +8,7 @@
 
         <a
           role="button"
-          href="https://drive.google.com/file/d/1NbFtr0kZOLp-eCIG-tuQk-Zg5NgC2Im9/view?usp=sharing"
+          href="https://drive.google.com/file/d/1NbFtr0kZOLp-eCIG-tuQk-Zg5NgC2Im9/view"
           target="_blank"
           rel="noopener noreferrer nofollow"
           class="border border-blue-600 py-3 px-2 rounded-lg hover:bg-blue-600 hover:text-white"
