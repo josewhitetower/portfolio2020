ca040c0b5f922cd8e0961dda7f1baa246907a058
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-12-19 18:30:10 +0100

    added experiences and fixed typo

--- content/experience/software-developer-d-vinci-hr-systems-gmb.md
+++ content/experience/software-developer-d-vinci-hr-systems-gmb.md
@@ -7,7 +7,7 @@ keywords: Jira, Pair Programming, Javascript
 company: d.vinci HR-Systems GmbH
 link: https://www.dvinci.de/
 task: Implemented and helped in the conception of new functionalities for
-  [d.vinci](https://www.dvinci.de/bewerbermanagement-software-dvinc) and also
+  [d.vinci](https://www.dvinci.de/bewerbermanagement-software-dvinci) and also
   customer-specific adjustments and extensions for it. It also included analysis
   and correction of software errors in cooperation with the customer Service
   using the [JIRA](https://www.atlassian.com/software/jira) ticket system.
--- experiences.js
+++ experiences.js
@@ -1,16 +1,26 @@
 export default {
   1: {
+    role: 'Software Developer',
+    company: {
+      name: 'S-KON eKontor 24 GmbH',
+      link: 'https://www.skon.de/'
+    },
+    duration: '01/2021 - Today',
+    tasks: 'TBD',
+    keywords: ['React.js', 'Styled Components', 'E-Commerce']
+  },
+  2: {
     role: 'Frontend Software Developer',
     company: {
       name: 'Spiegel TechLab GmbH',
       link: 'https://www.spiegel-techlab.de'
     },
-    duration: '09/2017 - Today',
+    duration: '09/2017 - 12/2020',
     tasks:
       '<p>As part of a cross-functional, dedicated and agile team, I contribute as developer in various digital publishing products (<a href="https://www.spiegel.de" target="_blank" rel="noopener noreferrer nofollow">Der Spiegel</a>, <a href="https://www.bento.de" target="_blank" rel="noopener noreferrer nofollow">Bento</a>, <a href="https://www.manager-magazin.de/" target="_blank" rel="noopener noreferrer nofollow">Manager Magazin</a>). Implemented responsive and functional layouts and worked in close cooperation with editors and mobile and backend developers. Mentor, assist, and review code for peer web developers.</p>',
     keywords: ['VueJs', 'TailwindCss', 'ComponentLoaderJs', 'Code Reviews']
   },
-  2: {
+  3: {
     role: 'Software Developer',
     company: {
       name: 'd.vinci HR-Systems GmbH ',
@@ -21,7 +31,7 @@ export default {
       '<p>Implemented and helped in the conception of new functionalities for <a href="https://www.dvinci.de/bewerbermanagement-software-dvinci" target="_blank" rel="noopener noreferrer nofollow">d.vinci</a> and also customer-specific adjustments and extensions for it. It also included analysis and correction of software errors in cooperation with the customer Service using the <a href="https://www.atlassian.com/software/jira" target="_blank" rel="noopener noreferrer nofollow">JIRA</a> ticket system. Practiced pair programming as learning strategy.</p>',
     keywords: ['Jira', 'Pair Programming', 'Javascript']
   },
-  3: {
+  4: {
     role: 'Internship as Software Developer',
     company: {
       name: 'd.vinci HR-Systems GmbH ',
